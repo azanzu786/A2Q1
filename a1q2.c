234b899913ecfad5293fb6fe5e4f767ee40eba9c
azanzu786 <95052895+azanzu786@users.noreply.github.com>
2025-02-27 23:24:31 -0500

    Update a1q2.c

--- a1q2.c
+++ a1q2.c
@@ -1,22 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
-
+    
 #define BUFSIZE 256
-
+    
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    if (argc != 2) {
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-
-    struct stat st;
-    if (stat(argv[1], &st) != 0) {
-        fprintf(stderr, "Error: file doesn't exist or is not accessible.\n");
-        return -1;
-    }
-
-    printf("%lld\n", (long long)st.st_size);
-    return 0;
+    char cmd[BUFSIZE] = "wc -c < ";
+    strcat(cmd, argv[1]);
+    system(cmd);
 }
